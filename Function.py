515f04579570b95782b0b8892b53f8261e86a261
RMAI-PLEUM <oyashi11110@gmail.com>
2021-09-01 11:56:50 +0700

    new test

---
+++ Function.py
@@ -0,0 +1,5 @@
+import math
+def calculate(a,b):
+    return a**b
+
+print(calculate(2,5))
---
+++ args.py
@@ -0,0 +1,7 @@
+def ListTrack(*args):
+    return args
+
+print(ListTrack("sdf",34,"324",555))
+mac = []
+mac =input("Enter your data : ").split(",")
+print(mac)
